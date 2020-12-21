926b69b1056a93c3bd4f288c8415d007b2d36a3c
raulvega26 <rvega@parlem.com>
2020-12-21 17:10:36 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './task-list/task-list.component.html',
+  styleUrls: ['./task-list/task-list.component.css']
+})
+export class AppComponent {
+  title = 'everis-dar-todo-list-front';
+}
